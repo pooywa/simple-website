d888fff2fb7a5f4bf35cdf4c8abd42ef844c0926
Pooya Shourangiz <pooyashourangiz@gmail.com>
2025-02-19 12:02:57 +0330

    Add some code for routing

--- 1.py
+++ 1.py
@@ -4,3 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World!'
+
+@app.route("/<name>")
+def userpage(name):
+    return f"Hi {name}!"
+
+if __name__ == '__main__':
+    app.run()
