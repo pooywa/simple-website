abae0addcbb73162d77cda9e15d7a7a82d427ddc
Pooya Shourangiz <pooyashourangiz@gmail.com>
2025-02-19 12:21:34 +0330

    Using redirect in functin as route

--- 1.py
+++ 1.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 @app.route('/')
-def index():
+def home():
     return 'Hello World!'
 
 @app.route("/<name>")
 def userpage(name):
     return f"Hi {name}!"
+
+@app.route("/first")
+def firstpage():
+    return redirect(url_for("userpage", name="pooya"))
 
 if __name__ == '__main__':
     app.run()
